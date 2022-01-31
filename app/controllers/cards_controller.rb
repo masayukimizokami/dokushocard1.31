96f561eb83e9c58013041aeb9d20c4c162e5f0c1
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-02-01 08:26:29 +0900

    index show edit new

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,45 @@
 class CardsController < ApplicationController
   def index
+    @cards = Card.all
   end
 
   def new
+    @card = Card.new
   end
 
   def edit
+    @card = Card.find(params[:id])
   end
 
   def show
+    @card = Card.find(params[:id])
   end
+
+  def create
+    @card = Card.new(params.require(:card).permit(:title, :id, :author, :memo, :price, :publisher))
+    if @card.save
+      flash[:notice] = "本を登録しました"
+      redirect_to :cards
+    else
+      flash[:notice] = "本を登録できませんでした"
+      render "new"
+    end
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(params.require(:card).permit(:title, :id, :author, :memo, :price, :publisher))
+      flash[:notice] = "本情報を更新しました"
+      redirect_to :cards
+    else
+      flash[:notice] = "本情報を更新できませんでした"
+      render "edit"
+    end
+  end
+
+  def delete
+    @card = Card.find(params:id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'cards/index'
-  get 'cards/new'
-  get 'cards/edit'
-  get 'cards/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources:cards
 end
