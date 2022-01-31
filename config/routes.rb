48ba06a00b5607417ebf282135f7eb0f620209e0
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-01-31 17:52:08 +0900

    読書カード作成

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards/new'
+  get 'cards/edit'
+  get 'cards/show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
